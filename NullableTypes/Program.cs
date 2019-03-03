9ee6220e90dc915c2a7f86b34255c19b221194da
Ankita <ankita.m@softwaysolutions.com>
2019-03-03 18:42:16 +0530

    POC on nullable types

---
+++ NullableTypes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullableTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? AreYouMajor = null;
+            Console.WriteLine("enter your first name: ");
+            Console.ReadLine();
+            Console.WriteLine("enter your last name: ");
+            Console.ReadLine();
+
+            Console.WriteLine("enter your age: ");
+            Console.ReadLine();
+            if (AreYouMajor >= 18)
+            {
+                Console.WriteLine("you are eligible to vote..");
+                Console.ReadLine();
+            }
+            else if (AreYouMajor <= 18)
+            {
+                Console.WriteLine("you are not eligible to vote...");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("user did not enter the value..");
+                Console.ReadLine();
+            }
+        }
+    }
+}
